44c65fdeacb351c3b5e98963fb2ed49d80ec2c13
Cora Zhu <zhiyi.zhu@lincolnuni.ac.nz>
2023-11-27 08:43:19 +0000

    LandUseChange_BuildUp pie chart completed

--- app.py
+++ app.py
@@ -2,45 +2,46 @@ from dash import Dash, html, dcc
 import pandas as pd
 import plotly.graph_objects as go
 
-def load_forest_land_use_data():
-    """Loads forest land use change data."""
-    return pd.read_csv("static/data/LandUseChange_Forest_1990_2016.csv")
+def load_builtup_area_data():
+    """Loads land use change data for built-up areas."""
+    return pd.read_csv("static/data/LandUseChange_BuildUp_1990_2016.csv")
 
-def prepare_forest_land_use_chart_data(data_df):
-    """Prepares data for the forest land use pie chart."""
-    land_use_data = data_df.iloc[0, 3:]  # land use columns start from the 4th column
-    labels = land_use_data.index.tolist()
-    values = land_use_data.values.tolist()
+def prepare_builtup_area_chart_data(data_df):
+    """Prepares data for the built-up area land use pie chart."""
+    # Extracting data from 'Forest' column onwards
+    builtup_area_data = data_df.iloc[0, 3:]
+    labels = builtup_area_data.index.tolist()
+    values = builtup_area_data.values.tolist()
     return labels, values
 
-def create_forest_land_use_pie_chart(labels, values):
-    """Creates a pie chart for forest land use data."""
+def create_builtup_area_pie_chart(labels, values):
+    """Creates a pie chart for built-up area land use data."""
     return go.Figure(data=[go.Pie(labels=labels, values=values)])
 
-def setup_dash_layout(app, fig_pie_chart):
-    """Sets up the layout of the Dash app."""
+def setup_builtup_area_layout(app, fig_pie_chart):
+    """Sets up the layout of the Dash app for built-up area visualization."""
     app.layout = html.Div(children=[
         html.Div([
-            dcc.Graph(id='forest-land-use-pie-chart', figure=fig_pie_chart)
+            dcc.Graph(id='builtup-area-pie-chart', figure=fig_pie_chart)
         ]),
         html.Div([  
-            html.H3(id='forest-land-use-pie-chart-description',children='Land uses converted from forestland since 1990.')
+            html.H3(id='builtup-area-pie-chart-description', children='What was all this urbanised land before 1990?')
         ])
-    ],id='forest-land-use-pie-chart-layout')
+    ], id='builtup-area-pie-chart-layout')
 
 def create_app():
-    """Creates and configures the Dash app."""
+    """Creates and configures the Dash app for built-up area data."""
     app = Dash(__name__)
 
     # Load and prepare data
-    data_df = load_forest_land_use_data()
-    labels, values = prepare_forest_land_use_chart_data(data_df)
+    data_df = load_builtup_area_data()
+    labels, values = prepare_builtup_area_chart_data(data_df)
 
     # Create pie chart
-    fig_pie_chart = create_forest_land_use_pie_chart(labels, values)
+    fig_pie_chart = create_builtup_area_pie_chart(labels, values)
 
     # Setup layout
-    setup_dash_layout(app, fig_pie_chart)
+    setup_builtup_area_layout(app, fig_pie_chart)
 
     return app
 
